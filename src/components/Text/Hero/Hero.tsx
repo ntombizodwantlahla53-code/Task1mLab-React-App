7e0b272affbc0981fa240be98af86c47f5a8b1a3
ntombizodwantlahla53-code <ntombizodwantlahla53@gmail.com>
2026-07-07 21:08:36 +0200

    Merge branch 'main' of https://github.com/ntombizodwantlahla53-code/gTask1mLab-React-App

--- src/components/Text/Hero/Hero.tsx
+++ src/components/Text/Hero/Hero.tsx
@@ -20,7 +20,6 @@ export const Hero = () => {
        <div className={styles.heroImage}>
        <img src={cofe} />
         </div>
-{/* Right Side: Coffee Image */}
       <div className={styles.iconRow}>
         <img src={coffeeIcon} />
         <img src={Icon} />
