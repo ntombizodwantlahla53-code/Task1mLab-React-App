fb3f792190ec0fb03de38f6a8a6d82dbc482f518
ntombizodwantlahla53 <ntombizodwantlahla53@gmail.com>
2026-07-07 16:04:42 +0200

    updatedApp

--- src/components/Text/Hero/Hero.tsx
+++ src/components/Text/Hero/Hero.tsx
@@ -7,32 +7,33 @@ import thirdIcon from '../../../assets/icons8-coffee-50-4.png'
 import anotherIcon from '../../../assets/icons8-coffee-50.png'
 import cofe from '../../../assets/heroo.jpg'
 
+
 export const Hero = () => {
   return (
     <section className={styles.hero}>
       <ContentContainer>
-       <Text variant="h1">Coffee</Text>
-         <Text variant="h1">The Best For You</Text>
-        <Text>
-         <button className={styles.learnMore}>Learn More</button></Text>
-       <div className={styles.categories}>
-       </div>
-       <div className={styles.heroImage}>
-       <img src={cofe} />
-        </div>
-{/* Right Side: Coffee Image */}
-      <div className={styles.iconRow}>
-        <img src={coffeeIcon} />
-        <img src={Icon} />
-        <img src={thirdIcon} />
-        <img src={anotherIcon} />
-            <img src="/../../../assets/heroo.jpg"  />
-            <img src="../../../assets/icons8-coffee-50-3.png" />
-            <img src="../../../assets/icons8-coffee-50-4.png" />
-            <img src="../../../assets/icons8-coffee-50.png"/>
+        <div className={styles.info}>
+          <div>
+       <Text variant="h1">Coffee</Text> 
+      
+      <Text>
+      <button className={styles.viewMenu}>View Menu</button></Text>
+         <div className={styles.iconRow}>
+            <img src={coffeeIcon} />
+            <img src={Icon} />
+            <img src={thirdIcon} />
+            <img src={anotherIcon} />
           </div>
-
-     </ContentContainer>
+|</div>
+         <img src={cofe} className={styles.heroImage}/>
+         
+          <div className={styles.left}>
+         
+          </div>
+          
+        </div>  
+        
+        </ContentContainer>
    </section>
   )
 }
